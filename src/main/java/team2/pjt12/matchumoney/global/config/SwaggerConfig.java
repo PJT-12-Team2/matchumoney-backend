32b9142d3d252f107a9166b02c591703caf564dc
MJ <nampink2300@gmail.com>
2025-07-23 17:34:24 +0900

    feat: Swagger에 Bearer 토큰 인증 설정 추가

--- src/main/java/team2/pjt12/matchumoney/global/config/SwaggerConfig.java
+++ src/main/java/team2/pjt12/matchumoney/global/config/SwaggerConfig.java
@@ -13,6 +13,7 @@ import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
