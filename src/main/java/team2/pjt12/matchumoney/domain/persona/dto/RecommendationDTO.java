11371e1080d248cc2b4083d2854ce0c65fded8b6
hyeongseob01 <heongseob11@gmail.com>
2025-07-23 11:26:23 +0900

    feat: 민감 정보 제거 및 병합 커밋 재작성

--- src/main/java/team2/pjt12/matchumoney/domain/persona/dto/RecommendationDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/persona/dto/RecommendationDTO.java
@@ -1,10 +1,8 @@
 package team2.pjt12.matchumoney.domain.persona.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class RecommendationDTO {
     private String title;
     private String detail;
