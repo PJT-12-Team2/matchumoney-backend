93d2598e55c040714e3cd54815c86182fbf5eccf
MJ <nampink2300@gmail.com>
2025-07-25 13:43:57 +0900

    fix: 적금상품을 식별하기 위하여 추가

--- src/main/java/team2/pjt12/matchumoney/domain/saving/dto/MySavingProductResponseDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/dto/MySavingProductResponseDTO.java
@@ -10,6 +10,9 @@ import lombok.Data;
 @AllArgsConstructor
 @ApiModel(description = "사용자가  적금 상품 정보 DTO")
 public class MySavingProductResponseDTO {
+    @ApiModelProperty(value = "적금 상품 id", example = "1")
+    private String id;
+
     @ApiModelProperty(value = "적금 상품 제목", example = "KB푸른바다적금")
     private String title;
 
