4a2ace5359a5a2faa7739d3cf780bdc9ff2b5b14
MJ <nampink2300@gmail.com>
2025-07-25 13:57:07 +0900

    chore: 설명을 위한 주석 추가

--- src/main/java/team2/pjt12/matchumoney/domain/saving/service/SavingAccountService.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/service/SavingAccountService.java
@@ -5,8 +5,11 @@ import team2.pjt12.matchumoney.domain.saving.dto.MySavingProductResponseDTO;
 
 import java.util.List;
 
+//controller 에서 사용하는 service
 public interface SavingAccountService {
-    //controller 에서 사용하는 service
-    List<MySavingProductResponseDTO> getSavingAccount();
+    List<MySavingProductResponseDTO> getSavingAccountList();
+
     List<MySavingProductResponseDTO> retrieveAccounts(BankLoginRequestDTO requestDto);
+
+    List<SavingListItemResponseDTO> getUserRecommendedSavingAccounts(Long id);
 }
