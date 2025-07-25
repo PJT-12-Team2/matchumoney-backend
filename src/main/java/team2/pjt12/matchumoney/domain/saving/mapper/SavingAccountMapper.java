73f07d9eb64baeca4946669c35455157047732ab
MJ <nampink2300@gmail.com>
2025-07-25 13:54:04 +0900

    refactor: 필요없는 import 제거

--- src/main/java/team2/pjt12/matchumoney/domain/saving/mapper/SavingAccountMapper.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/mapper/SavingAccountMapper.java
@@ -4,7 +4,6 @@ package team2.pjt12.matchumoney.domain.saving.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import team2.pjt12.matchumoney.domain.saving.domain.SavingAccountVO;
-import org.apache.ibatis.annotations.Param;
 import team2.pjt12.matchumoney.domain.saving.dto.MySavingProductResponseDTO;
 import team2.pjt12.matchumoney.domain.saving.dto.SavingListItemResponseDTO;
 
@@ -13,6 +12,7 @@ import java.util.List;
 @Mapper
 public interface SavingAccountMapper {
     void deleteByUserIdAndFinId(@Param("userId") Long userId, @Param("finId") Long finId);
+
     void insertSavingAccount(SavingAccountVO savingAccountVO);
 
     MySavingProductResponseDTO getSavingAccount(@Param("id") Long id);
