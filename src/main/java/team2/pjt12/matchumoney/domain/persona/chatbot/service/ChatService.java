ec7fc31edf310eb34450475f8e76827027137340
hyeongseob01 <heongseob11@gmail.com>
2025-07-25 11:34:21 +0900

    fix : chatbot 기능 피드백 반영, cardlist 수정

--- src/main/java/team2/pjt12/matchumoney/domain/persona/chatbot/service/ChatService.java
+++ src/main/java/team2/pjt12/matchumoney/domain/persona/chatbot/service/ChatService.java
@@ -1,22 +1,18 @@
 package team2.pjt12.matchumoney.domain.persona.chatbot.service;
 
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team2.pjt12.matchumoney.domain.persona.chatbot.dto.ChatResponse;
 import team2.pjt12.matchumoney.domain.persona.chatbot.util.OpenAIClient;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
 
     private final OpenAIClient openAIClient;
-
-    public ChatService(OpenAIClient openAIClient) {
-        this.openAIClient = openAIClient;
-    }
 
     public ChatResponse askGPT(String message) {
         String reply = openAIClient.callChatGPT(message);
         return new ChatResponse(reply);
     }
 }
-
