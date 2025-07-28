f52107c3b4b324b59ed6dd475680ec53934b6612
tjdgus3488 <tjdgus3488@naver.com>
2025-07-28 21:38:58 +0900

    chore: 에러 메시지 수정

--- src/main/java/team2/pjt12/matchumoney/global/exception/ErrorCode.java
+++ src/main/java/team2/pjt12/matchumoney/global/exception/ErrorCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     // 공통 예외
-    INVALID_INPUT_VALUE(400, "C400", "잘못된 입력입니다."),
+    INVALID_INPUT_VALUE(400, "C400", "비밀번호의 형식이 맞지 않습니다."),
     METHOD_NOT_ALLOWED(405, "C405", "허용되지 않은 HTTP 메서드입니다."),
     INTERNAL_SERVER_ERROR(500, "C500", "서버 오류가 발생했습니다."),
     RESOURCE_NOT_FOUND(404, "C404", "요청한 리소스를 찾을 수 없습니다."),
