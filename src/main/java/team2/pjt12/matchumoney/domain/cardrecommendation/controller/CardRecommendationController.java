74f3063565f6dbc6e274201adbf3825d88d49128
byesun <rkdqudtjs6@naver.com>
2025-08-01 09:20:45 +0900

    fix: 매핑 버전 제거

--- src/main/java/team2/pjt12/matchumoney/domain/cardrecommendation/controller/CardRecommendationController.java
+++ src/main/java/team2/pjt12/matchumoney/domain/cardrecommendation/controller/CardRecommendationController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/card-recommendation")
+@RequestMapping("/api/card-recommendation")
 @RequiredArgsConstructor
 public class CardRecommendationController {
 
