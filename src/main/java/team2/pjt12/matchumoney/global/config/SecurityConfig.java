b806322aa1d1d9fd40b5e6c9c5ec81ae3a87fd8e
hyeongseob01 <heongseob11@gmail.com>
2025-08-05 16:34:10 +0900

    수정사항 변경

--- src/main/java/team2/pjt12/matchumoney/global/config/SecurityConfig.java
+++ src/main/java/team2/pjt12/matchumoney/global/config/SecurityConfig.java
@@ -64,14 +64,11 @@ public class SecurityConfig {
                                         "/api/deposits/**",
                                         "/api/deposit/**",
                                         "/api/cards/**",
-<<<<<<< Updated upstream
                                         "/api/persona/**",
-                                        "/api/users/me/**"
-=======
+                                        "/api/users/me/**",
                                         "/api/deposit-products/**",
                                         "/api/saving-products/**",
                                         "/api/card-products/**"
->>>>>>> Stashed changes
                                 ).permitAll()  // 허용 URL 설정
                                 .requestMatchers("/user/update").authenticated()
                                 .anyRequest().authenticated()  // 그 외 모든 요청은 인증 필요
