22eecc6fcbda744c3655bf057d3c27e9cfe9013e
byesun <rkdqudtjs6@naver.com>
2025-08-05 19:19:22 +0900

    feat: 카카오 response 변경

--- src/main/java/team2/pjt12/matchumoney/domain/auth/dto/LoginResponseDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/auth/dto/LoginResponseDTO.java
@@ -1,13 +1,14 @@
 package team2.pjt12.matchumoney.domain.auth.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LoginResponseDTO {
 
     private final String accessToken;
-
-    public LoginResponseDTO(String accessToken) {
-        this.accessToken = accessToken;
-    }
+    private final Long userId;
+    private final String nickname;
+    private final Long personaId;
 }
