2968d4547de597542f8b6088cb65f7df56a4d3d4
byesun <rkdqudtjs6@naver.com>
2025-08-07 18:32:02 +0900

    feat: 문제 추천 및 문제 mapper

---
+++ src/main/java/team2/pjt12/matchumoney/domain/quiz/mapper/QuizProblemMapper.java
@@ -0,0 +1,31 @@
+package team2.pjt12.matchumoney.domain.quiz.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import team2.pjt12.matchumoney.domain.quiz.domain.QuizProblemVO;
+
+import java.util.List;
+
+@Mapper
+public interface QuizProblemMapper {
+
+    List<QuizProblemVO> findAll();
+    
+    QuizProblemVO findById(@Param("problemId") Long problemId);
+    
+    void save(QuizProblemVO quizProblem);
+    
+    void update(QuizProblemVO quizProblem);
+    
+    void deleteById(@Param("problemId") Long problemId);
+    
+    QuizProblemVO findRandomProblem();
+    
+    List<QuizProblemVO> findUnsolvedProblemsByUserId(@Param("userId") Long userId);
+    
+    QuizProblemVO findRandomUnsolvedProblem(@Param("userId") Long userId);
+    
+    int getTotalCount();
+    
+    int getUnsolvedCountByUserId(@Param("userId") Long userId);
+}
