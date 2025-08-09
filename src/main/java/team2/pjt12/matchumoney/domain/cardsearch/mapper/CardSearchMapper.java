682346e9a058cd17cae89bba4d0813c4c89293e0
MJ <nampink2300@gmail.com>
2025-08-09 14:18:59 +0900

    fix: 카드 필터링 조회 좋아요&즐겨찾기 구현

--- src/main/java/team2/pjt12/matchumoney/domain/cardsearch/mapper/CardSearchMapper.java
+++ src/main/java/team2/pjt12/matchumoney/domain/cardsearch/mapper/CardSearchMapper.java
@@ -1,6 +1,7 @@
 package team2.pjt12.matchumoney.domain.cardsearch.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import team2.pjt12.matchumoney.domain.cardsearch.dto.CardSearchRequestDTO;
 import team2.pjt12.matchumoney.domain.cardsearch.dto.CardSearchResponseDTO;
 
@@ -9,5 +10,7 @@ import java.util.List;
 @Mapper
 public interface CardSearchMapper {
 
-    List<CardSearchResponseDTO> selectCardsByFilter(CardSearchRequestDTO request);
+    List<CardSearchResponseDTO> selectCardsByFilter(@Param("request") CardSearchRequestDTO request, @Param("userId") Long userId);
 }
+
+
