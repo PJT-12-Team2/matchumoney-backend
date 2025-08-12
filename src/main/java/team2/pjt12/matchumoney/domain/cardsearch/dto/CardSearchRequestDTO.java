44e45ab54c1eab666fab5c7e8d3119dcb5d35ec4
maedachaeva <parkchaeyeon.b@gmail.com>
2025-08-12 10:49:50 +0900

    feat: 카드 검색 무한스크롤 API 구현

--- src/main/java/team2/pjt12/matchumoney/domain/cardsearch/dto/CardSearchRequestDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/cardsearch/dto/CardSearchRequestDTO.java
@@ -1,5 +1,7 @@
 package team2.pjt12.matchumoney.domain.cardsearch.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +11,14 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "카드 검색 요청 파라미터")
 public class CardSearchRequestDTO {
+    @ApiModelProperty(value = "신용카드 포함 여부", example = "true")
     private boolean creditCard;
+
+    @ApiModelProperty(value = "체크카드 포함 여부", example = "true")
     private boolean debitCard;
+
+    @ApiModelProperty(value = "선택한 혜택명 목록", example = "[\"교통\", \"카페/디저트\", \"통신\"]")
     private List<String> selectedBenefits;
 }
