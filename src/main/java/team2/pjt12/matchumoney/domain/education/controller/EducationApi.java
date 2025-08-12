6c102ecdb6a5b8b4f7febfa547d35d7e73ed3bed
MJ <nampink2300@gmail.com>
2025-08-12 15:53:24 +0900

    feat: 교육 관련 Controller 구현(영상)

---
+++ src/main/java/team2/pjt12/matchumoney/domain/education/controller/EducationApi.java
@@ -0,0 +1,21 @@
+package team2.pjt12.matchumoney.domain.education.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import team2.pjt12.matchumoney.domain.education.dto.EduMovieResponseDTO;
+
+import java.util.List;
+
+@Api(tags = "Education Api", description = "교육자료 API")
+@RequestMapping("/api/education")
+public interface EducationApi {
+
+    @ApiOperation(value = "교육 영상 조회", notes = "DB에 저장된 교육 영상 정보를 조회합니다.")
+    @GetMapping("/movie")
+    ResponseEntity<List<EduMovieResponseDTO>> getEducationVideos(Long offset, int pageSize);
+
+
+}
