d1a48b0c6047f8bcafd7012d78456bd876637abe
MJ <nampink2300@gmail.com>
2025-08-16 15:41:16 +0900

    feat: CODEF 관련 API CodefController로 이동

--- src/main/java/team2/pjt12/matchumoney/domain/saving/controller/SavingAccountApi.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/controller/SavingAccountApi.java
@@ -4,25 +4,24 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import team2.pjt12.matchumoney.domain.saving.dto.BankLoginRequestDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import team2.pjt12.matchumoney.domain.saving.dto.MySavingProductResponseDTO;
 import team2.pjt12.matchumoney.domain.saving.dto.SavingListItemResponseDTO;
 
 import java.util.List;
 
-@Api(tags = "SavingAccount", description = "예적금 계좌 관련 API")
+@Api(tags = "My SavingAccount API", description = "내 적금 계좌 관련 API")
 @RequestMapping("/api/user/me/savings")
 public interface SavingAccountApi {
 
-    @ApiOperation(value = "예적금 계좌 동기화 및 조회", notes = "은행 로그인 정보로 사용자의 예적금 계좌를 동기화 후 조회합니다.")
-    @PostMapping("/sync")
-    ResponseEntity<List<MySavingProductResponseDTO>> syncSavingAccount(
-            @ApiParam(value = "은행 로그인 요청 DTO") @RequestBody BankLoginRequestDTO requestDto);
 
-    @ApiOperation(value = "예적금 계좌 조회", notes = "DB에 저장된 사용자의 예적금 계좌 정보를 조회합니다.")
+    @ApiOperation(value = "적금 계좌 조회", notes = "DB에 저장된 사용자의 적금 계좌 정보를 조회합니다.")
     @GetMapping("")
     ResponseEntity<List<MySavingProductResponseDTO>> getMySavingAccount();
+
 
     @ApiOperation(value = "추천 적금 리스트 (페이징)", notes = "특정 적금 계좌 기반 추천 적금 리스트를 페이징으로 반환")
     @GetMapping("/{id}/recommend")
@@ -31,4 +30,6 @@ public interface SavingAccountApi {
             @ApiParam(value = "페이지 번호", defaultValue = "0") @RequestParam(defaultValue = "0") int page,
             @ApiParam(value = "페이지 크기", defaultValue = "10") @RequestParam(defaultValue = "10") int size
     );
+
+
 }
