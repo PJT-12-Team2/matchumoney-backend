079a5cb99aadb6739746776fd000da571faca182
MJ <nampink2300@gmail.com>
2025-08-16 23:14:39 +0900

    feat: builder 등 어노테이션 추가

--- src/main/java/team2/pjt12/matchumoney/domain/saving/dto/BankLoginRequestDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/dto/BankLoginRequestDTO.java
@@ -1,15 +1,26 @@
 package team2.pjt12.matchumoney.domain.saving.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
+@Setter
 @Getter
+@AllArgsConstructor
+@Builder
 public class BankLoginRequestDTO {
+    @ApiModelProperty(value = "은행 번호", example = "0004", required = true)
+    private String bankCode;
     @ApiModelProperty(value = "사용자 은행 아이디", example = "STAR11112", required = true)
     private String id;
     @ApiModelProperty(value = "사용자 은행 비밀번호", example = "12345!", required = true)
     private String password;
     @ApiModelProperty(value = "사용자 생년월일", example = "20250730", required = true)
     private String birthDate;
-//    private String bankName; // ex: "국민은행"
+
+    public BankLoginRequestDTO() {
+
+    }
 }
