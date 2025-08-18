6994e0f9df881b3ff4becf0bb8c70f966ac08099
MJ <nampink2300@gmail.com>
2025-08-18 23:04:21 +0900

    feat: swagger 설명 추가

--- src/main/java/team2/pjt12/matchumoney/domain/savingdetail/dto/LikeStatusResponseDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/savingdetail/dto/LikeStatusResponseDTO.java
@@ -1,5 +1,7 @@
 package team2.pjt12.matchumoney.domain.savingdetail.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "LikeStatusResponse", description = "적금 상품 좋아요 상태 응답 DTO")
 public class LikeStatusResponseDTO {
+
+    @ApiModelProperty(value = "좋아요 여부", example = "true")
     private boolean liked;
+
+    @ApiModelProperty(value = "좋아요 개수", example = "25")
     private int likeCount;
 }
