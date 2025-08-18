86c38e4c990008d5adcc01f7d95f6aa9a2ebe8cf
MinJu <98680218+namminju@users.noreply.github.com>
2025-08-18 10:37:49 +0900

    Update SwaggerConfig.java

--- src/main/java/team2/pjt12/matchumoney/global/config/SwaggerConfig.java
+++ src/main/java/team2/pjt12/matchumoney/global/config/SwaggerConfig.java
@@ -1,7 +1,8 @@
-package team2.pjt12.matchumoney.global.config; 
+package team2.pjt12.matchumoney.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -39,7 +40,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("team2.pjt12.matchumoney.domain.saving"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
