914d35b65a0edb485538a37832bd7090055c9a84
MJ <nampink2300@gmail.com>
2025-08-19 00:09:26 +0900

    feat: swagger 설명 추가

--- src/main/java/team2/pjt12/matchumoney/domain/savingsearch/dto/SavingOptionDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/savingsearch/dto/SavingOptionDTO.java
@@ -5,11 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-<<<<<<< HEAD
 @ApiModel(value = "SavingOption", description = "적금 상품 금리 정보 DTO")
-=======
-@ApiModel(value = "SavingOption", description = "적금 상품 옵션 정보 DTO")
->>>>>>> 687abec (feat: swagger 설명 추가)
 public class SavingOptionDTO {
     @ApiModelProperty(value = "저축 기간 (개월)", example = "12")
     private String saveTrm;
@@ -17,10 +13,7 @@ public class SavingOptionDTO {
     @ApiModelProperty(value = "기본 금리", example = "2.3")
     private Double intrRate;
 
-<<<<<<< HEAD
-    @ApiModelProperty(value = "최대 금리", example = "3.55")
-=======
+
     @ApiModelProperty(value = "최대 금리", example = "3.5")
->>>>>>> 687abec (feat: swagger 설명 추가)
     private Double intrRate2;
 }
