44619a1c2657ebd0ec4c703cdda3b2883045d949
maedachaeva <parkchaeyeon.b@gmail.com>
2025-08-18 19:46:00 +0900

    feat: cardrecommendation swagger 설명 추가

--- src/main/java/team2/pjt12/matchumoney/domain/cardrecommendation/dto/CardRecommendationResponseDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/cardrecommendation/dto/CardRecommendationResponseDTO.java
@@ -1,5 +1,7 @@
 package team2.pjt12.matchumoney.domain.cardrecommendation.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.util.List;
@@ -9,9 +11,17 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(description = "더 나은 카드 추천 응답 DTO")
 public class CardRecommendationResponseDTO {
-    private Long totalSpendAmount; // 최근 30일 총 사용금액
-    private List<CategoryStatDTO> categoryStats; // 카테고리별 사용 통계
-    private List<CardBenefitDTO> recommendedCards; // 추천 카드 목록 (상위 5개)
-    private String message; // 추천 메시지
+    @ApiModelProperty(value = "최근 30일 총 사용금액", example = "485000")
+    private Long totalSpendAmount;
+
+    @ApiModelProperty(value = "카테고리별 사용 통계")
+    private List<CategoryStatDTO> categoryStats;
+
+    @ApiModelProperty(value = "추천 카드 목록(상위 5개)")
+    private List<CardBenefitDTO> recommendedCards;
+
+    @ApiModelProperty(value = "추천 메시지")
+    private String message;
 }
