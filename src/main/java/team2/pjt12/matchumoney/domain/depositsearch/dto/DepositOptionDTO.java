b5e9e075df8f6ec72baa349edaf1b2b9c1a245ed
maedachaeva <parkchaeyeon.b@gmail.com>
2025-08-19 00:53:24 +0900

    feat: depositsearch swagger 설명 추가

--- src/main/java/team2/pjt12/matchumoney/domain/depositsearch/dto/DepositOptionDTO.java
+++ src/main/java/team2/pjt12/matchumoney/domain/depositsearch/dto/DepositOptionDTO.java
@@ -1,10 +1,18 @@
 package team2.pjt12.matchumoney.domain.depositsearch.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description = "예금 금리 옵션 DTO")
 public class DepositOptionDTO {
+    @ApiModelProperty(value = "기간(개월)", example = "12")
     private String saveTrm;
+
+    @ApiModelProperty(value = "기본 금리(%)", example = "2.50")
     private Double intrRate;
+
+    @ApiModelProperty(value = "최대 금리(%)", example = "2.90")
     private Double intrRate2;
 }
