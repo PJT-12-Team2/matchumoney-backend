c3b66ea00be3c585e400276773bb077a77aff3b2
MJ <nampink2300@gmail.com>
2025-08-18 23:19:42 +0900

    feat: swagger 설명 추가

--- src/main/java/team2/pjt12/matchumoney/domain/saving/codef/controller/CodefApi.java
+++ src/main/java/team2/pjt12/matchumoney/domain/saving/codef/controller/CodefApi.java
@@ -15,28 +15,28 @@ import java.util.List;
 @RequestMapping("/api/codef")
 public interface CodefApi {
 
-    @ApiOperation(value = "예적금 계좌 동기화 및 조회", notes = "은행 로그인 정보로 사용자의 예적금 계좌를 동기화 후 조회합니다.")
+    @ApiOperation(value = "예적금 계좌 동기화 및 조회(처음)", notes = "은행 로그인 정보로 사용자의 예적금 계좌를 동기화 후 조회합니다.")
     @PostMapping("/saving/sync")
     ResponseEntity<List<MySavingProductResponseDTO>> syncSavingAccount(
             @ApiParam(value = "은행 로그인 요청 DTO") @RequestBody BankLoginRequestDTO requestDto);
 
-    @ApiOperation(value = "예적금 계좌 동기화 및 조회", notes = "이전의 정보로 사용자의 예적금 계좌를 동기화 후 조회합니다.")
+    @ApiOperation(value = "예적금 계좌 동기화 및 조회(재연결)", notes = "저장된 정보로 사용자의 예적금 계좌를 동기화 후 조회합니다.")
     @PostMapping("/saving/sync/pre")
     ResponseEntity<List<MySavingProductResponseDTO>> syncSavingAccountPre();
 
-    @ApiOperation(value = "연결된 connectedId 리스트", notes = "")
+    @ApiOperation(value = "연결된 connectedId 리스트", notes = "테스트용 api 입니다.")
     @GetMapping("")
     ResponseEntity<List<String>> getConnectedIdList();
 
-    @ApiOperation(value = "coneectedId 제거", notes = "")
+    @ApiOperation(value = "coneectedId 제거", notes = "연결되어 있던 은행 (로그인) 정보를 제거합니다.")
     @DeleteMapping("/connectedId")
     ResponseEntity<String> deleteConnectedId();
 
-    @ApiOperation(value = "connectedId 업데이트", notes = "")
+    @ApiOperation(value = "connectedId 업데이트", notes = "연결된 은행 정보를 업데이트합니다. (로그인 정보가 변경된 경우)")
     @PutMapping("/connectedId")
     ResponseEntity<List<MySavingProductResponseDTO>> updateConnectedId(@ApiParam(value = "은행 로그인 요청 DTO") @RequestBody BankLoginRequestDTO requestDto);
 
-    @ApiOperation(value = "connectedId에 연결된 은행 리스트", notes = "")
+    @ApiOperation(value = "connectedId에 연결된 은행 리스트", notes = "테스트용 api 입니다.")
     @GetMapping("/connectedId")
     ResponseEntity<List<String>> getBanksByConnectedId();
 
